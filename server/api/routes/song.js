34ecf3e9eb55aa28a7e7947d6ad0ce8d89fc828f
Yuko_Yoshida <yukoyoshida614@gmail.com>
2019-09-28 11:32:34 +0900

    POST song/upload作成。

--- server/api/routes/song.js
+++ server/api/routes/song.js
@@ -2,6 +2,23 @@ const express = require('express')
 const router = express.Router()
 const routerAuth = express.Router()
 const Model = require('../models')
+const multer = require('multer')
+const fs = require('fs')
+
+
+const storage = multer.diskStorage({
+  destination: function(req, res, cb) {
+    const dist = __dirname+'/uploads'
+    if (!fs.existsSync(dist)) fs.mkdirSync(dist)
+    cb(null, dist)
+  }
+})
+
+const upload = multer({ storage: storage }).fields([
+  { name: 'song', maxCount: 1 },
+  { name: 'songInfo', maxCount: 1 },
+  { name: 'artwork', maxCount: 1 },
+])
 
 
 router.get('/', (req, res) => {
@@ -19,6 +36,29 @@ router.get('/', (req, res) => {
       }
     })
     res.status(200).json(songInfos)
+  })
+})
+
+routerAuth.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) return res.status(400).send()
+    if (!req.body.songInfo) return res.status(400).send()
+    if (!req.files.song) return res.status(400).send()
+
+    const body = JSON.parse(req.body.songInfo)
+    const artwork = (req.files.artwork) ? req.files.artwork[0].filename : ''
+    const songInfo = {
+      artist: body.artist,
+      title: body.title,
+      album: body.album,
+      artwork: artwork,
+      filename: req.files.song[0].filename
+    }
+
+    return Model('Song').create((err) => {
+      if (err) return res.status(400).send()
+      return res.status(200).send()
+    })
   })
 })
 
