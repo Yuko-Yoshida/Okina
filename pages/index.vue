581fa3cbfe102092f969af22a381eebf6d82f3ca
Yuko_Yoshida <yukoyoshida614@gmail.com>
2019-10-01 15:34:47 +0900

    aplayer

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,28 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        music-platform
-      </h1>
-      <h2 class="subtitle">
-        My wondrous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <no-ssr>
+      <aplayer preload="matadata"
+        :music="{
+          title: 'secret base~君がくれたもの~',
+          artist: 'Silent Siren',
+          src: 'http://localhost:3000/api/v2/song/5d92e96b3560c6b13d5491a4/audio',
+          pic: 'http://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
+        }"
+      />
+    </no-ssr>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Aplayer from 'vue-aplayer'
 
 export default {
   components: {
-    Logo
+    Aplayer
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ plugins/vue-aplayer.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import aplayer from 'vue-aplayer'
+
+Vue.use(aplayer)
