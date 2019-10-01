dbb89142e7216ddf03923bffbcba298d3059efe1
Yuko_Yoshida <yukoyoshida614@gmail.com>
2019-10-01 16:31:12 +0900

    レイアウト #32

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="container">
-    <no-ssr>
-      <aplayer preload="matadata"
-        :music="{
-          title: 'secret base~君がくれたもの~',
-          artist: 'Silent Siren',
-          src: 'http://localhost:3000/api/v2/song/5d92e96b3560c6b13d5491a4/audio',
-          pic: 'http://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
-        }"
-      />
-    </no-ssr>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="112" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+    <div class="columns">
+      <div class="column is-two-thirds">
+        <no-ssr>
+          <aplayer preload="matadata"
+            :music="{
+              title: '',
+              artist: '',
+              src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
+              pic: ''
+            }"
+          />
+        </no-ssr>
+        <no-ssr>
+          <aplayer preload="matadata"
+            :music="{
+              title: '',
+              artist: '',
+              src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
+              pic: ''
+            }"
+          />
+        </no-ssr>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">John Smith</p>
+                <p class="subtitle is-6">@johnsmith</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br>
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
